33aa7c9e6c3ff7dfae08375df47f3f0e748e4ed5
kint@ <frkinta@gmail.com>
2021-05-10 21:25:48 +0100

    createStreamFromFile on files without upload error (#43)
    * createStreamFromFile on files without upload error
    
    * Update ServerRequestCreator.php
    
    * Update ServerRequestCreator.php
    
    * code formatting
    
    * Check if upload is okey
    
    Co-authored-by: Nyholm <tobias.nyholm@gmail.com>

--- src/ServerRequestCreator.php
+++ src/ServerRequestCreator.php
@@ -212,10 +212,14 @@ final class ServerRequestCreator implements ServerRequestCreatorInterface
             return $this->normalizeNestedFileSpec($value);
         }
 
-        try {
-            $stream = $this->streamFactory->createStreamFromFile($value['tmp_name']);
-        } catch (\RuntimeException $e) {
+        if (UPLOAD_ERR_OK !== $value['error']) {
             $stream = $this->streamFactory->createStream();
+        } else {
+            try {
+                $stream = $this->streamFactory->createStreamFromFile($value['tmp_name']);
+            } catch (\RuntimeException $e) {
+                $stream = $this->streamFactory->createStream();
+            }
         }
 
         return $this->uploadedFileFactory->createUploadedFile(
